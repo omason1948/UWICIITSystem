fdddce2dc107468d8d6f2beeefb874395b983620
omason1948 <49859660+omason1948@users.noreply.github.com>
2020-05-12 15:09:43 -0400

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -842,7 +842,7 @@ def insurance():
             return "complete 2"
 
         #db.insurance.insert_one(form.data)
-        db.insurance.insert_one({"studentId": form.data["studentId"],"studentName":form.data["studentName"], "studentEmail":form.data["studentEmail"], "insurancePeriod":form.data["insurancePeriod"], "payment": directory + "/" + filename})
+        db.insurance.insert_one({"studentId": form.data["userId"],"studentName":form.data["studentName"], "studentEmail":form.data["studentEmail"], "insurancePeriod":form.data["insurancePeriod"], "payment": directory + "/" + filename})
         return "complete"
         
     return render_template('insurance.html', title='Insurance', form=form, userId=userId, data=data, user=username, email=email)
