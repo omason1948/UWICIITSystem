490cda5cd2c6bed9105159aeb039acd4036f356f
romariorenee@gmail.com <romariorenee@gmail.com>
2020-05-13 14:27:02 -0400

    Change Upload Folder

--- app/routes.py
+++ app/routes.py
@@ -849,7 +849,7 @@ def insurance():
         if file and allowed_file(file.filename):
 
             filename = secure_filename(file.filename)
-            path = os.path.join(os.path.abspath('app/userphotos'))
+            path = os.path.join(os.path.abspath('app/static/insurance'))
             file.save(os.path.join(path, secure_filename(filename)))
 
         db.insurance.insert_one({"studentId": userId, "insurancePeriod": form.data['insurancePeriod'], "payment": filename})
@@ -915,7 +915,7 @@ def eventsadd():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             
-            path = os.path.join(os.path.abspath('app/userphotos'))
+            path = os.path.join(os.path.abspath('app/static/userphotos'))
             file.save(os.path.join(path, secure_filename(filename)))
 
             #file.save(os.path.join(app.config['UPLOAD_FOLDER'] + "/" + directory , filename))
