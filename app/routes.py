9b6c50f3fe64cc255ca71a9d2102e5123502e4db
omason1948 <49859660+omason1948@users.noreply.github.com>
2020-06-12 22:35:42 -0400

    Test2

--- app/routes.py
+++ app/routes.py
@@ -1313,7 +1313,7 @@ def admin_students_view(studentid):
 
     registered_courses = db.registration.find({"studentID":student_data["UserID"]})
 #     personal_info = db.student.find({"studentId":student_data["UserID"]})
-    personal_info = db.student.find({"studentId":12345})
+    personal_info = db.student.find({"studentId":student_data["UserID"]})
     
     return render_template('admin_student_view.html', title = 'Admin Student Information', student_data = student_data, UserProfile = UserProfile, registered_courses = registered_courses, user = username, personal_info=personal_info)
 
