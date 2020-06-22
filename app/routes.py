08562096b568c2a9574041a7844c93a0fca6178d
omason1948 <49859660+omason1948@users.noreply.github.com>
2020-06-22 16:41:36 -0400

    Add }

--- app/routes.py
+++ app/routes.py
@@ -1617,7 +1617,7 @@ def admin_insurance_status_update(studentId):
     insuranceform = InsuranceForm()
     insurance_details = db.insurance.find_one({'_id': ObjectId(studentId)})
     if request.method == 'POST':
-        db.insurance.update_one({'_id': ObjectId(studentId), {"$set":{'insuranceStatus': insuranceform.data['insuranceStatus']}})
+        db.insurance.update_one({'_id': ObjectId(studentId)}, {"$set":{'insuranceStatus': insuranceform.data['insuranceStatus']}})
 
     return render_template(
         'admin_insurance_status_update.html',
