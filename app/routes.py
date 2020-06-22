e45df213519a244419ea3aa14bdfdce4c230f769
BlueXANA <33667126+BlueXANA@users.noreply.github.com>
2020-06-22 16:12:38 -0400

    Added event edit date validation

--- app/routes.py
+++ app/routes.py
@@ -1044,6 +1044,10 @@ def eventsedit(id):
     data = db.events.find({"_id" : ObjectId(id)})
 
     if request.method =='POST':
+	eventDate=form.data["eventDate"]
+        if eventDate.date() < date.today():
+            flash("The date entered is in the past!")
+            return render_template('event-edit.html', title='Edit Events', form = form, user = username)
         file = request.files['photo']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
